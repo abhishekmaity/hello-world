d06d8204e89b12e03ff18d26c9b0be0f876f8a73
Abhishek Maity <abhishekmaity@users.noreply.github.com>
2015-09-15 19:55:29 +0530

    Create HelloWorld.c

---
+++ HelloWorld.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+
+int main(){               //Beginning of main() function of return type int
+
+  printf("Hello World");  //Procedure to display 'Hello World'
+  
+return 0;
+}                         //End of main() function
